b5f51d8f0e4370cdd0a16069eb6d78e7386ec9f6
Andrius Alimas <alimasWebDeveloper@gmail.com>
2021-10-15 14:45:00 +0100

    Update script.js result table
    Update script.js result table: if we get success from ajax call then we create result table with fetch data from api

--- task/js/script.js
+++ task/js/script.js
@@ -19,16 +19,42 @@ $("#weather_api").click(()=>{
     },
     success: function(result){
       // for testing only to see result in console
-      console.log(JSON.stringify(result));
-      console.log(result['data']['countryCode']);
+      // console.log(JSON.stringify(result));
       if (result.status.name == "ok") {
-       $('#result').html("<h1>" + result['data']['countryCode']+"</h1>");
+       $('#result').html("<table class='table table-bordered table-info'><caption>Results</caption>"+
+                            "<thead class='text-success'>" +
+                              "<tr>" +
+                                "<th>Date Time</th>" + 
+                                "<th>Station Name</th>" + 
+                                "<th>Country Code</th>" + 
+                                "<th>Temperature</th>" + 
+                                "<th>Clouds</th>" + 
+                                "<th>Wind Direction</th>" + 
+                                "<th>Wind Speed</th>" + 
+                                "<th>Humidity</th>" + 
+                                "<th>Weather Condition</th>" + 
+                                "</tr>" + 
+                            "</thead>" +
+                            "<tbody>" +
+                                "<tr>" +
+                                  "<td>"+result['data']['datetime'] +"</td>"+
+                                  "<td>"+result['data']['stationName'] +"</td>"+
+                                  "<td>"+result['data']['countryCode'] +"</td>"+
+                                  "<td>"+result['data']['temperature'] +"</td>"+
+                                  "<td>"+result['data']['clouds'] +"</td>"+
+                                  "<td>"+result['data']['windDirection'] +"</td>"+
+                                  "<td>"+result['data']['windSpeed'] +"</td>"+
+                                  "<td>"+result['data']['humidity'] +"</td>"+
+                                  "<td>"+result['data']['weatherCondition'] +"</td>"+
+                                "</tr>"+
+                            "</tbody>"+
+                          "</table>"
+       );
       }
     },
-
     error: function(jqXHR, textStatus, errorThrown) {
       // your error code
-      $('#result').html("<p class='bg-danger text-white'>Please make sure you entered Latitude and Longitude fields, these fields are mandatory!</p>");
+      $('#result').html("<p class='bg-danger text-white'>Please make sure you entered Latitude and Longitude fields, these fields are mandatory!Make sure radius catch area!</p>");
     }
   })
 });
