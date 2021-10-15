8936feccb76f2d78ca86bd8606b53d71aa2bb329
Andrius Alimas <alimasWebDeveloper@gmail.com>
2021-10-15 11:32:33 +0100

    Update script.js with ajax call
    Update script.js with ajax call: created ajax call to php file, expected return json

--- task/js/script.js
+++ task/js/script.js
@@ -6,8 +6,24 @@ $(window).on('load', function () {
   }
 })
 
-
 // ajax call when pressed weather api button
 $("#weather_api").click(()=>{
-  alert("button clicked!");
+  $.ajax({
+    url: "php/getWeatherObservation.php",
+    type: 'POST',
+    dataType: 'json',
+    success: function(result){
+      // for testing only to see result in console
+      console.log(JSON.stringify(result));
+
+      if (result.status.name == "ok") {
+        $("#result").html("<h1>Yes</h1>");
+      }
+    },
+
+    error: function(jqXHR, textStatus, errorThrown) {
+      // your error code
+      console.log("No");
+    }
+  })
 });
