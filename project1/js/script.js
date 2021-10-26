ab7c553125872803708cda671325e5044d5b276f
Andrius Alimas <alimasWebDeveloper@gmail.com>
2021-10-26 14:49:17 +0100

    Hide nav and main element using opacity
    Hide nav and main element using opacity

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,7 +1,8 @@
 $(window).on('load', function () {
     if ($('.loading-area').length) {
-    $('.loading-area').delay(1000).fadeOut('slow', function () {
-    $(this).remove();
+        $('.loading-area').delay(1000).fadeOut('slow', function () {
+        $(this).hide();
+        $('nav, main').css('opacity',1);
    });
   }
 })
