8b4b5316232f185d5f664da3cb553c57553e8e28
Andrius Alimas <alimasWebDeveloper@gmail.com>
2021-10-27 11:54:42 +0100

    Update getApi.php
    Update getApi.php:
    added check server if its local or not then in curl will be different path because it accept absolute path.
    
    added case in switch that checks which curl api path need to use and how to process.

--- project1/php/getApi.php
+++ project1/php/getApi.php
@@ -1,13 +1,25 @@
 <?php
+
+    // check if server local or not
+  $server = $_SERVER['SERVER_NAME'];
+    if($server === "localhost")
+    {
+        $server .= '/andriusAlimas';
+    }
+
+     $show = true; // just variable to check formating
+     $data = 'data';
     ini_set('display_errors', 'On');
     error_reporting(E_ALL);
 
     $executionStartTime = microtime(true);
     $api_name = $_REQUEST['api_name'];
-    
-    // switch($api_name){
-       
-    // }
+    switch($api_name){
+       case 'countryBorders':
+        $show = false;
+        $url = 'http://'.$server.'/project1/json/countryBorders.geo.json';
+        break;
+    }
     
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -15,7 +27,6 @@
     curl_setopt($ch, CURLOPT_URL,$url);
 
     $result=curl_exec($ch);
-    
     curl_close($ch);
  
     $decode = json_decode($result,true);    
@@ -24,8 +35,13 @@
     $output['status']['description'] = "success";
     $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
     
+    // $output[$data] = $decode;
+ // because some decode is different we need to make sure its right format
+ if($show == true){ 
     $output['data'] = $decode[$api_name];
- 
+}else{
+    $output[$data] = $decode;   
+}
     header('Content-Type: application/json; charset=UTF-8');
 
     echo json_encode($output);
