5fcad956e789046693cc8cd79af5e0160fd0333b
Andrius Alimas <alimasWebDeveloper@gmail.com>
2021-10-27 11:55:47 +0100

    ajax call when page load

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,12 +1,33 @@
+// when loading execute this function
 $(window).on('load',  () =>{
     if ($('.loading-area').length) {
         $('.loading-area').delay(1000).fadeOut('slow', function () {
         $(this).hide();
         $('nav, main').css('opacity',1);
+        $('select').removeAttr("disabled");
    });
   }
 })
 
 // when all html loaded execute this function
 $(document).ready(()=>{
-});
+
+    $.ajax({
+      url: "php/getApi.php",
+      type: 'POST',
+      dataType: 'json',
+      data: {
+            "api_name": "countryBorders"
+          },
+      success: function(result){
+        console.log("yes");
+        result['data'].features.forEach(function (feature) {
+          $("<option>",{
+            value: feature.properties.iso_a2,
+            text: feature.properties.name
+          }).appendTo("#countrySelect");
+        })
+      }
+    });
+    // navigator.geolocation.getCurrentPosition(getUserLocation);
+  });
