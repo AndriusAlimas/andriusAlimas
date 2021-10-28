38833ad2d160297f710224f0e352eab14e1df42e
Andrius Alimas <alimasWebDeveloper@gmail.com>
2021-10-28 14:05:03 +0100

    Major update script.js selecting and drawing borders
    Fix some code from var to let using, created countryBorderLayer and assigned to map. Then on this layer added selected country and draw polygons

--- project1/js/script.js
+++ project1/js/script.js
@@ -5,6 +5,8 @@ $(window).on('load',  () =>{
         $(this).hide();
         $('nav, main').css('opacity',1);
         $('select').removeAttr("disabled");
+        navigator.geolocation.getCurrentPosition(setMap);
+
    });
   }
 })
@@ -28,8 +30,6 @@ $(document).ready(()=>{
         sortCountries();
       }
     });
-     // load and set map at current user position
-     navigator.geolocation.getCurrentPosition(setMap);
 });
 
 // FUNCTIONS
@@ -45,12 +45,15 @@ const  sortCountries = () =>{
 
 // set map using user locations
 const  setMap = (position) =>{
-    var userPositionlat = position.coords.latitude;
-    var userPositionlng = position.coords.longitude;
+    let userPositionlat = position.coords.latitude;
+    let userPositionlng = position.coords.longitude;
 
     // access map position and view
-    let map = L.map('mapView').setView([userPositionlat,userPositionlng], 10);
-  
+    map = L.map('mapView').setView([userPositionlat,userPositionlng], 6);
+
+   // add layer for selected country
+   countryBorderLayer = L.layerGroup().addTo(map);
+
     // call ajax call to access token and draw a main map
     $.ajax({
         url: 'php/getApi.php',
@@ -84,3 +87,38 @@ const  setMap = (position) =>{
       }
       })
 }
+
+const drawCountryBorders = (feature_collection, iso_a2) =>{
+    countryBorderLayer.clearLayers(); // clear layer to redraw new 
+
+     // filter country borders matched selected one and get feature object
+     let countryGeoJSONBorder = feature_collection['features'].filter((a) => (a.properties.iso_a2 === iso_a2));
+
+    // add to border layer 
+    L.geoJSON(countryGeoJSONBorder).addTo(countryBorderLayer);
+}
+
+// Event trigger select change
+$('#countrySelect').change(function() {
+    let iso_a2 = $('#countrySelect option:selected').val();
+    
+    $.ajax({
+        url: "php/getApi.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            "api_name": "countryBorders",
+            iso: iso_a2
+        },
+        success: function(result) {
+
+            if (result.status.name == "ok") {
+                 drawCountryBorders(result['data'],iso_a2);
+            }
+    
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+        }
+    });
+});
